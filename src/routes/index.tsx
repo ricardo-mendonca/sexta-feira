562cdd5d973a3e4f18b5b1df42d075f5af4c1f25
Ricardo Mendonca <ricardo-mendonca@live.com>
2023-12-04 13:05:02 -0300

    feat: configuracao menu Drawer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,29 @@
-import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import {  useDrawerContext } from '../shared/contexts';
+import { Button } from '@mui/material';
+
+import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-  const{toggleDrawerOpen} = useDrawerContext();
+  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        icon: 'home',
+        path: '/pagina-inicial',
+        label: 'Página inicial',
+      },
+    ]);
+  }, []);
+
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>teste</Button> } />
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
-}
+};
+
+
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,47 +1,81 @@
 import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 import { Box } from '@mui/system';
 
 import { useDrawerContext } from '../../contexts';
+import React from 'react';
 
-interface IAppThemeProviderProps {children: React.ReactNode}
+//#region Criação dos botoes do Menu
+interface IListItemLinkProps {
+    to: string;
+    icon: string;
+    label: string;
+    onClick: (() => void) | undefined;
+}
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
+    const navigate = useNavigate();
+    const resolvedPath = useResolvedPath(to);
+    const match = useMatch({ path: resolvedPath.pathname, end: false });
 
+    const handleClick = () => {
+        navigate(to);
+        onClick?.();
+    };
+
+    return (
+        <ListItemButton selected={!!match} onClick={handleClick}>
+            <ListItemIcon>
+                <Icon>{icon}</Icon>
+            </ListItemIcon>
+            <ListItemText primary={label} />
+        </ListItemButton>
+    );
+};
+//#endregion
+
+
+interface IAppThemeProviderProps { children: React.ReactNode }
 export const MenuLateral: React.FC<IAppThemeProviderProps> = ({ children }) => {
-  const theme = useTheme();
-  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+    const { isDrawerOpen, drawerOptions, toggleDrawerOpen } = useDrawerContext();
 
-  return (
-    <>
-      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
-        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+    return (
+        <>
+            <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
+                <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
 
-          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
-            <Avatar
-              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
-              src="broken-image.jpg"
-            />
-          </Box>
+                    <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+                        <Avatar
+                            sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+                            src="broken-image.jpg"
+                        />
+                    </Box>
 
-          <Divider />
+                    <Divider />
 
-          <Box flex={1}>
-            <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Página inicial" />
-              </ListItemButton>
-            </List>
-          </Box>
+                    <Box flex={1}>
+                        <List component="nav">
+                            {drawerOptions.map(drawerOption => (
+                                <ListItemLink
+                                    to={drawerOption.path}
+                                    key={drawerOption.path}
+                                    icon={drawerOption.icon}
+                                    label={drawerOption.label}
+                                    onClick={smDown ? toggleDrawerOpen : undefined}
+                                />
+                            ))}
 
-        </Box>
-      </Drawer>
+                        </List>
+                    </Box>
 
-      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
-        {children}
-      </Box>
-    </>
-  );
+                </Box>
+            </Drawer>
+
+            <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
+                {children}
+            </Box>
+        </>
+    );
 };
