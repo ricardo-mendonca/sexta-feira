a390f4c86e6d38326e4ea4e276f36035382fdafd
Ricardo Mendonca <ricardo-mendonca@live.com>
2023-12-04 15:26:40 -0300

    feat: configurando Layout responsivo

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -4,8 +4,8 @@ import { LayoutBaseDePagina } from '../../shared/layouts';
 export const Dashboard = () => {
 
   return (
-    <LayoutBaseDePagina titulo='Página inicial'>
-      Testando
-    </LayoutBaseDePagina>
+    <LayoutBaseDePagina titulo='Página inicial' barraDeFerramentas={<>Barra de ferramentas</>}>
+    Testando
+  </LayoutBaseDePagina>
   );
 };
--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,37 +1,49 @@
+import { ReactNode } from 'react';
 import { Icon, IconButton, Theme, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 
 import { useDrawerContext } from '../contexts';
 
-interface IAppThemeProviderProps { children: React.ReactNode, titulo: string; }
-export const LayoutBaseDePagina: React.FC<IAppThemeProviderProps> = ({ children, titulo }) => {
-    const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
-    const theme = useTheme();
+interface IAppThemeProviderProps { 
+    children: React.ReactNode ;
+    titulo: string;
+  barraDeFerramentas?: ReactNode;
+}
+export const LayoutBaseDePagina: React.FC<IAppThemeProviderProps> = ({ children, titulo, barraDeFerramentas }) => {
+  const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+  const theme = useTheme();
 
-    const { toggleDrawerOpen } = useDrawerContext();
+  const { toggleDrawerOpen } = useDrawerContext();
 
-    return (
-        <Box height="100%" display="flex" flexDirection="column" gap={1}>
-           
-            <Box padding={1} display="flex" alignItems="center" height={theme.spacing(12)} gap={1}>
-                {smDown && (
-                    <IconButton onClick={toggleDrawerOpen}>
-                        <Icon>menu</Icon>
-                    </IconButton>
-                )}
+  return (
+    <Box height="100%" display="flex" flexDirection="column" gap={1}>
+      <Box padding={1} display="flex" alignItems="center" gap={1} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}>
+        {smDown && (
+          <IconButton onClick={toggleDrawerOpen}>
+            <Icon>menu</Icon>
+          </IconButton>
+        )}
 
-                <Typography variant="h5">
-                    {titulo}
-                </Typography>
-            </Box>
+        <Typography
+          overflow="hidden"
+          whiteSpace="nowrap"
+          textOverflow="ellipses"
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+        >
+          {titulo}
+        </Typography>
+      </Box>
 
-            <Box>
-                Barra de ferramentas
-            </Box>
-
-            <Box>
-                {children}
-            </Box>
+      {barraDeFerramentas && (
+        <Box>
+          {barraDeFerramentas}
         </Box>
-    );
+      )}
+
+      <Box flex={1} overflow="auto">
+        {children}
+      </Box>
+    </Box>
+  );
 };
