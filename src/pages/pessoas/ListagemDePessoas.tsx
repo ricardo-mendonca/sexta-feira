a0b6f596ae88e7952c2077af4437209fb412b9f6
Ricardo Mendonca <ricardo-mendonca@live.com>
2023-12-08 17:07:25 -0300

    feat: Consultando server e retornando dados

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,21 +1,39 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { useDebounce } from '../../shared/hooks';
 
 
-export const ListagemDeCidade: React.FC = () => {
+export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(1000, false);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
 
+  useEffect(() => {
+
+    debounce(() => {
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log(result);
+          }
+        });
+    });
+  }, [busca]);
+
+
   return (
     <LayoutBaseDePagina
-      titulo='Listagem de cidades'
+      titulo='Listagem de pessoas'
       barraDeFerramentas={
         <FerramentasDaListagem
           mostrarInputBusca
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { useDrawerContext } from '../shared/contexts';
-import { Dashboard, ListagemDeCidade } from '../pages';
+import { Dashboard, ListagemDePessoas } from '../pages';
 
 export const AppRoutes = () => {
   const { setDrawerOptions } = useDrawerContext();
@@ -15,9 +15,9 @@ export const AppRoutes = () => {
         label: 'Página inicial',
       },
       {
-        icon: 'location_city',
-        path: '/cidades',
-        label: 'Cidades',
+        icon: 'people',
+        path: '/pessoas',
+        label: 'Pessoas',
       },
     ]);
   }, []);
@@ -26,7 +26,7 @@ export const AppRoutes = () => {
     <Routes>
       <Route path="/pagina-inicial" element={<Dashboard />} />
 
-      <Route path="/cidades" element={<ListagemDeCidade />} />
+      <Route path="/pessoas" element={<ListagemDePessoas />} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
